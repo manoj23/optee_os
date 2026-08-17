manoj23/optee_os

commit 9e91a619a03fd01c1744986a56843f4da105a060

ta: pkcs11: Add support for digest operations
Implements support for digest operations as specified in:

PKCS #11 Cryptographic Token Interface Base Specification Version 2.40
Plus Errata 01
5.10 Message digesting functions

Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Ruchika Gupta <ruchika.gupta@linaro.org>
Signed-off-by: Vesa Jääskeläinen <vesa.jaaskelainen@vaisala.com>

ta/pkcs11/src/processing_digest.c
@@ -1,0 +1,209 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2021, Vaisala Oyj
+ */
+
+#include <assert.h>
+#include <pkcs11_ta.h>
+#include <string.h>
+#include <tee_api_defines.h>
+#include <tee_internal_api.h>
+#include <tee_internal_api_extensions.h>
+#include <utee_defines.h>
+#include <util.h>
+
+#include "attributes.h"
+#include "object.h"
+#include "pkcs11_attributes.h"
+#include "pkcs11_helpers.h"
+#include "pkcs11_token.h"
+#include "processing.h"
+#include "serializer.h"
+
+bool processing_is_tee_digest(enum pkcs11_mechanism_id mecha_id)
+{
+	switch (mecha_id) {
+	case PKCS11_CKM_MD5:
+	case PKCS11_CKM_SHA_1:
+	case PKCS11_CKM_SHA224:
+	case PKCS11_CKM_SHA256:
+	case PKCS11_CKM_SHA384:
+	case PKCS11_CKM_SHA512:
+		return true;
+	default:
+		return false;
+	}
+}
+
+static enum pkcs11_rc
+pkcs2tee_algorithm(uint32_t *tee_id, struct pkcs11_attribute_head *proc_params)
+{
+	static const struct {
+		enum pkcs11_mechanism_id mech_id;
+		uint32_t tee_id;
+	} pkcs2tee_algo[] = {
+		{ PKCS11_CKM_MD5, TEE_ALG_MD5 },
+		{ PKCS11_CKM_SHA_1, TEE_ALG_SHA1 },
+		{ PKCS11_CKM_SHA224, TEE_ALG_SHA224 },
+		{ PKCS11_CKM_SHA256, TEE_ALG_SHA256 },
+		{ PKCS11_CKM_SHA384, TEE_ALG_SHA384 },
+		{ PKCS11_CKM_SHA512, TEE_ALG_SHA512 },
+	};
+	size_t n = 0;
+
+	for (n = 0; n < ARRAY_SIZE(pkcs2tee_algo); n++) {
+		if (proc_params->id == pkcs2tee_algo[n].mech_id) {
+			*tee_id = pkcs2tee_algo[n].tee_id;
+			return PKCS11_CKR_OK;
+		}
+	}
+
+	return PKCS11_RV_NOT_IMPLEMENTED;
+}
+
+static enum pkcs11_rc
+allocate_tee_operation(struct pkcs11_session *session,
+		       struct pkcs11_attribute_head *params)
+{
+	uint32_t algo = 0;
+	TEE_Result res = TEE_ERROR_GENERIC;
+
+	assert(session->processing->tee_op_handle == TEE_HANDLE_NULL);
+
+	if (pkcs2tee_algorithm(&algo, params))
+		return PKCS11_CKR_FUNCTION_FAILED;
+
+	res = TEE_AllocateOperation(&session->processing->tee_op_handle,
+				    algo, TEE_MODE_DIGEST, 0);
+	if (res)
+		EMSG("TEE_AllocateOp. failed %#"PRIx32, algo);
+
+	if (res == TEE_ERROR_NOT_SUPPORTED)
+		return PKCS11_CKR_MECHANISM_INVALID;
+
+	return tee2pkcs_error(res);
+}
+
+enum pkcs11_rc init_digest_operation(struct pkcs11_session *session,
+				     struct pkcs11_attribute_head *proc_params)
+{
+	assert(processing_is_tee_digest(proc_params->id));
+
+	return allocate_tee_operation(session, proc_params);
+}
+
+/*
+ * step_digest_operation - processing digest operation step
+ *
+ * @session - current session
+ * @step - step ID in the processing (oneshot, update, final)
+ * @obj - PKCS#11 object for key based operations
+ * @ptype - invocation parameter types
+ * @params - invocation parameter references
+ */
+enum pkcs11_rc step_digest_operation(struct pkcs11_session *session,
+				     enum processing_step step,
+				     struct pkcs11_object *obj,
+				     uint32_t ptypes, TEE_Param *params)
+{
+	enum pkcs11_rc rc = PKCS11_CKR_GENERAL_ERROR;
+	TEE_Result res = TEE_ERROR_GENERIC;
+	void *in_buf = NULL;
+	size_t in_size = 0;
+	void *out_buf = NULL;
+	uint32_t out_size = 0;
+	void *secret_value = NULL;
+	uint32_t secret_value_size = 0;
+	enum pkcs11_key_type key_type = PKCS11_CKK_UNDEFINED_ID;
+	struct active_processing *proc = session->processing;
+
+	if (TEE_PARAM_TYPE_GET(ptypes, 1) == TEE_PARAM_TYPE_MEMREF_INPUT) {
+		in_buf = params[1].memref.buffer;
+		in_size = params[1].memref.size;
+		if (in_size && !in_buf)
+			return PKCS11_CKR_ARGUMENTS_BAD;
+	}
+	if (TEE_PARAM_TYPE_GET(ptypes, 2) == TEE_PARAM_TYPE_MEMREF_OUTPUT) {
+		out_buf = params[2].memref.buffer;
+		out_size = params[2].memref.size;
+		if (out_size && !out_buf)
+			return PKCS11_CKR_ARGUMENTS_BAD;
+	}
+	if (TEE_PARAM_TYPE_GET(ptypes, 3) != TEE_PARAM_TYPE_NONE)
+		return PKCS11_CKR_ARGUMENTS_BAD;
+
+	switch (step) {
+	case PKCS11_FUNC_STEP_ONESHOT:
+	case PKCS11_FUNC_STEP_UPDATE:
+	case PKCS11_FUNC_STEP_UPDATE_KEY:
+	case PKCS11_FUNC_STEP_FINAL:
+		break;
+	default:
+		TEE_Panic(step);
+		break;
+	}
+
+	assert(proc->tee_op_handle != TEE_HANDLE_NULL);
+
+	assert(processing_is_tee_digest(proc->mecha_type));
+
+	/*
+	 * Feed active operation with data
+	 */
+
+	switch (step) {
+	case PKCS11_FUNC_STEP_UPDATE_KEY:
+		assert(obj);
+
+		if (get_class(obj->attributes) != PKCS11_CKO_SECRET_KEY)
+			return PKCS11_CKR_KEY_INDIGESTIBLE;
+
+		key_type = get_key_type(obj->attributes);
+
+		if (key_type != PKCS11_CKK_GENERIC_SECRET &&
+		    key_type != PKCS11_CKK_AES)
+			return PKCS11_CKR_KEY_INDIGESTIBLE;
+
+		rc = get_attribute_ptr(obj->attributes, PKCS11_CKA_VALUE,
+				       &secret_value, &secret_value_size);
+		assert(!rc && secret_value && secret_value_size);
+
+		TEE_DigestUpdate(proc->tee_op_handle, secret_value,
+				 secret_value_size);
+		return PKCS11_CKR_OK;
+
+	case PKCS11_FUNC_STEP_UPDATE:
+		if (!in_buf || !in_size)
+			return PKCS11_CKR_OK;
+
+		TEE_DigestUpdate(proc->tee_op_handle, in_buf, in_size);
+		return PKCS11_CKR_OK;
+
+	case PKCS11_FUNC_STEP_ONESHOT:
+		if (!out_buf)
+			return PKCS11_CKR_ARGUMENTS_BAD;
+
+		goto do_final;
+
+	case PKCS11_FUNC_STEP_FINAL:
+		if (in_buf || !out_buf)
+			return PKCS11_CKR_ARGUMENTS_BAD;
+
+		goto do_final;
+
+	default:
+		TEE_Panic(step);
+		break;
+	}
+
+do_final:
+	res = TEE_DigestDoFinal(proc->tee_op_handle,
+				in_buf, in_size, out_buf,
+				&out_size);
+	rc = tee2pkcs_error(res);
+
+	if (rc == PKCS11_CKR_OK || rc == PKCS11_CKR_BUFFER_TOO_SMALL)
+		params[2].memref.size = out_size;
+
+	return rc;
+}

ta/pkcs11/include/pkcs11_ta.h
@@ -597,6 +597,66 @@ enum pkcs11_ta_cmd {
 	 * processing matches.
 	 */
 	PKCS11_CMD_RELEASE_ACTIVE_PROCESSING = 44,
+
+	/*
+	 * PKCS11_CMD_DIGEST_INIT - Initialize a digest computation processing
+	 *
+	 * [in]  memref[0] = [
+	 *              32bit session handle,
+	 *              (struct pkcs11_attribute_head)mechanism + mecha params
+	 *	 ]
+	 * [out] memref[0] = 32bit return code, enum pkcs11_rc
+	 *
+	 * This command relates to the PKCS#11 API function C_DigestInit().
+	 */
+	PKCS11_CMD_DIGEST_INIT = 45,
+
+	/*
+	 * PKCS11_CMD_DIGEST_KEY - Update digest with a key
+	 *
+	 * [in]  memref[0] = [
+	 *              32bit session handle,
+	 *              32bit key handle
+	 *	 ]
+	 * [out] memref[0] = 32bit return code, enum pkcs11_rc
+	 *
+	 * This command relates to the PKCS#11 API function C_DigestKey().
+	 */
+	PKCS11_CMD_DIGEST_KEY = 46,
+
+	/*
+	 * PKCS11_CMD_DIGEST_UPDATE - Update digest with data
+	 *
+	 * [in]  memref[0] = 32bit session handle
+	 * [out] memref[0] = 32bit return code, enum pkcs11_rc
+	 * [in]  memref[1] = input data to be processed
+	 *
+	 * This command relates to the PKCS#11 API function C_DigestUpdate().
+	 */
+	PKCS11_CMD_DIGEST_UPDATE = 47,
+
+	/*
+	 * PKCS11_CMD_DIGEST_FINAL - Finalize a digest computation processing
+	 *
+	 * [in]  memref[0] = 32bit session handle
+	 * [out] memref[0] = 32bit return code, enum pkcs11_rc
+	 * [out] memref[2] = output digest
+	 *
+	 * This command relates to the PKCS#11 API function C_DigestFinal().
+	 */
+	PKCS11_CMD_DIGEST_FINAL = 48,
+
+	/*
+	 * PKCS11_CMD_DIGEST_ONESHOT - Compute a digest
+	 *
+	 * [in]  memref[0] = 32bit session handle
+	 * [out] memref[0] = 32bit return code, enum pkcs11_rc
+	 * [in]  memref[1] = input data to be processed
+	 * [out] memref[2] = byte array: generated digest
+	 *
+	 * This command relates to the PKCS#11 API function C_Digest().
+	 */
+	PKCS11_CMD_DIGEST_ONESHOT = 49,
 };
 
 /*
@@ -625,6 +685,7 @@ enum pkcs11_rc {
 	PKCS11_CKR_KEY_HANDLE_INVALID		= 0x0060,
 	PKCS11_CKR_KEY_SIZE_RANGE		= 0x0062,
 	PKCS11_CKR_KEY_TYPE_INCONSISTENT	= 0x0063,
+	PKCS11_CKR_KEY_INDIGESTIBLE		= 0x0067,
 	PKCS11_CKR_KEY_FUNCTION_NOT_PERMITTED	= 0x0068,
 	PKCS11_CKR_KEY_NOT_WRAPPABLE		= 0x0069,
 	PKCS11_CKR_KEY_UNEXTRACTABLE		= 0x006a,
@@ -1060,11 +1121,17 @@ enum pkcs11_key_type {
  * Note that this will be extended as needed.
  */
 enum pkcs11_mechanism_id {
+	PKCS11_CKM_MD5				= 0x00210,
 	PKCS11_CKM_MD5_HMAC			= 0x00211,
+	PKCS11_CKM_SHA_1			= 0x00220,
 	PKCS11_CKM_SHA_1_HMAC			= 0x00221,
+	PKCS11_CKM_SHA256			= 0x00250,
 	PKCS11_CKM_SHA256_HMAC			= 0x00251,
+	PKCS11_CKM_SHA224			= 0x00255,
 	PKCS11_CKM_SHA224_HMAC			= 0x00256,
+	PKCS11_CKM_SHA384			= 0x00260,
 	PKCS11_CKM_SHA384_HMAC			= 0x00261,
+	PKCS11_CKM_SHA512			= 0x00270,
 	PKCS11_CKM_SHA512_HMAC			= 0x00271,
 	PKCS11_CKM_GENERIC_SECRET_KEY_GEN	= 0x00350,
 	PKCS11_CKM_AES_KEY_GEN			= 0x01080,

ta/pkcs11/src/entry.c
@@ -322,6 +322,30 @@ TEE_Result TA_InvokeCommandEntryPoint(void *tee_session, uint32_t cmd,
 	case PKCS11_CMD_RELEASE_ACTIVE_PROCESSING:
 		rc = entry_release_active_processing(client, ptypes, params);
 		break;
+	case PKCS11_CMD_DIGEST_INIT:
+		rc = entry_processing_init(client, ptypes, params,
+					   PKCS11_FUNCTION_DIGEST);
+		break;
+	case PKCS11_CMD_DIGEST_UPDATE:
+		rc = entry_processing_step(client, ptypes, params,
+					   PKCS11_FUNCTION_DIGEST,
+					   PKCS11_FUNC_STEP_UPDATE);
+		break;
+	case PKCS11_CMD_DIGEST_KEY:
+		rc = entry_processing_step(client, ptypes, params,
+					   PKCS11_FUNCTION_DIGEST,
+					   PKCS11_FUNC_STEP_UPDATE_KEY);
+		break;
+	case PKCS11_CMD_DIGEST_ONESHOT:
+		rc = entry_processing_step(client, ptypes, params,
+					   PKCS11_FUNCTION_DIGEST,
+					   PKCS11_FUNC_STEP_ONESHOT);
+		break;
+	case PKCS11_CMD_DIGEST_FINAL:
+		rc = entry_processing_step(client, ptypes, params,
+					   PKCS11_FUNCTION_DIGEST,
+					   PKCS11_FUNC_STEP_FINAL);
+		break;
 	default:
 		EMSG("Command %#"PRIx32" is not supported", cmd);
 		return TEE_ERROR_NOT_SUPPORTED;

ta/pkcs11/src/pkcs11_attributes.c
@@ -106,6 +106,16 @@ check_mechanism_against_processing(struct pkcs11_session *session,
 		allowed = !mechanism_is_one_shot_only(mechanism_type);
 		break;
 
+	case PKCS11_FUNC_STEP_UPDATE_KEY:
+		assert(function == PKCS11_FUNCTION_DIGEST);
+
+		if (session->processing->always_authen &&
+		    !session->processing->relogged)
+			return PKCS11_CKR_USER_NOT_LOGGED_IN;
+
+		allowed = true;
+		break;
+
 	case PKCS11_FUNC_STEP_FINAL:
 		if (session->processing->always_authen &&
 		    !session->processing->relogged)

ta/pkcs11/src/pkcs11_attributes.h
@@ -113,6 +113,7 @@ enum processing_step {
 	PKCS11_FUNC_STEP_INIT,
 	PKCS11_FUNC_STEP_ONESHOT,
 	PKCS11_FUNC_STEP_UPDATE,
+	PKCS11_FUNC_STEP_UPDATE_KEY,
 	PKCS11_FUNC_STEP_FINAL,
 };
 

ta/pkcs11/src/pkcs11_helpers.c
@@ -182,6 +182,11 @@ static const struct any_id __maybe_unused string_ta_cmd[] = {
 	PKCS11_ID(PKCS11_CMD_GENERATE_RANDOM),
 	PKCS11_ID(PKCS11_CMD_DERIVE_KEY),
 	PKCS11_ID(PKCS11_CMD_RELEASE_ACTIVE_PROCESSING),
+	PKCS11_ID(PKCS11_CMD_DIGEST_INIT),
+	PKCS11_ID(PKCS11_CMD_DIGEST_UPDATE),
+	PKCS11_ID(PKCS11_CMD_DIGEST_KEY),
+	PKCS11_ID(PKCS11_CMD_DIGEST_ONESHOT),
+	PKCS11_ID(PKCS11_CMD_DIGEST_FINAL),
 };
 
 static const struct any_id __maybe_unused string_slot_flags[] = {
@@ -270,6 +275,7 @@ static const struct any_id __maybe_unused string_rc[] = {
 	PKCS11_ID(PKCS11_CKR_USER_TOO_MANY_TYPES),
 	PKCS11_ID(PKCS11_CKR_USER_TYPE_INVALID),
 	PKCS11_ID(PKCS11_CKR_KEY_SIZE_RANGE),
+	PKCS11_ID(PKCS11_CKR_KEY_INDIGESTIBLE),
 	PKCS11_ID(PKCS11_CKR_SESSION_READ_ONLY_EXISTS),
 	PKCS11_ID(PKCS11_CKR_SIGNATURE_LEN_RANGE),
 	PKCS11_ID(PKCS11_RV_NOT_FOUND),
@@ -312,6 +318,7 @@ static const struct any_id __maybe_unused string_internal_processing[] = {
 };
 
 static const struct any_id __maybe_unused string_functions[] = {
+	PKCS11_ID(PKCS11_FUNCTION_DIGEST),
 	PKCS11_ID(PKCS11_FUNCTION_IMPORT),
 	PKCS11_ID(PKCS11_FUNCTION_ENCRYPT),
 	PKCS11_ID(PKCS11_FUNCTION_DECRYPT),
@@ -563,6 +570,9 @@ void pkcs2tee_mode(uint32_t *tee_id, enum processing_func function)
 		break;
 	case PKCS11_FUNCTION_DERIVE:
 		*tee_id = TEE_MODE_DERIVE;
+		break;
+	case PKCS11_FUNCTION_DIGEST:
+		*tee_id = TEE_MODE_DIGEST;
 		break;
 	default:
 		TEE_Panic(function);

ta/pkcs11/src/processing.c
@@ -46,6 +46,11 @@ static enum processing_func func_for_cmd(enum pkcs11_ta_cmd cmd)
 	case PKCS11_CMD_VERIFY_UPDATE:
 	case PKCS11_CMD_VERIFY_FINAL:
 		return PKCS11_FUNCTION_VERIFY;
+	case PKCS11_CMD_DIGEST_UPDATE:
+	case PKCS11_CMD_DIGEST_KEY:
+	case PKCS11_CMD_DIGEST_ONESHOT:
+	case PKCS11_CMD_DIGEST_FINAL:
+		return PKCS11_FUNCTION_DIGEST;
 	default:
 		return PKCS11_FUNCTION_UNKNOWN;
 	}
@@ -337,9 +342,11 @@ enum pkcs11_rc entry_processing_init(struct pkcs11_client *client,
 	if (rc)
 		return rc;
 
-	rc = serialargs_get(&ctrlargs, &key_handle, sizeof(uint32_t));
-	if (rc)
-		return rc;
+	if (function != PKCS11_FUNCTION_DIGEST) {
+		rc = serialargs_get(&ctrlargs, &key_handle, sizeof(uint32_t));
+		if (rc)
+			return rc;
+	}
 
 	rc = serialargs_alloc_get_one_attribute(&ctrlargs, &proc_params);
 	if (rc)
@@ -354,10 +361,12 @@ enum pkcs11_rc entry_processing_init(struct pkcs11_client *client,
 	if (rc)
 		goto out;
 
-	obj = pkcs11_handle2object(key_handle, session);
-	if (!obj) {
-		rc = PKCS11_CKR_KEY_HANDLE_INVALID;
-		goto out;
+	if (function != PKCS11_FUNCTION_DIGEST) {
+		obj = pkcs11_handle2object(key_handle, session);
+		if (!obj) {
+			rc = PKCS11_CKR_KEY_HANDLE_INVALID;
+			goto out;
+		}
 	}
 
 	rc = set_processing_state(session, function, obj, NULL);
@@ -370,17 +379,23 @@ enum pkcs11_rc entry_processing_init(struct pkcs11_client *client,
 	if (rc)
 		goto out;
 
-	rc = check_parent_attrs_against_processing(proc_params->id, function,
-						   obj->attributes);
-	if (rc)
-		goto out;
+	if (obj) {
+		rc = check_parent_attrs_against_processing(proc_params->id,
+							   function,
+							   obj->attributes);
+		if (rc)
+			goto out;
 
-	rc = check_access_attrs_against_token(session, obj->attributes);
-	if (rc)
-		goto out;
+		rc = check_access_attrs_against_token(session,
+						      obj->attributes);
+		if (rc)
+			goto out;
+	}
 
 	if (processing_is_tee_symm(proc_params->id))
 		rc = init_symm_operation(session, function, proc_params, obj);
+	else if (processing_is_tee_digest(proc_params->id))
+		rc = init_digest_operation(session, proc_params);
 	else
 		rc = PKCS11_CKR_MECHANISM_INVALID;
 
@@ -419,6 +434,8 @@ enum pkcs11_rc entry_processing_step(struct pkcs11_client *client,
 	struct serialargs ctrlargs = { };
 	struct pkcs11_session *session = NULL;
 	enum pkcs11_mechanism_id mecha_type = PKCS11_CKM_UNDEFINED_ID;
+	uint32_t key_handle = 0;
+	struct pkcs11_object *obj = NULL;
 
 	if (!client ||
 	    TEE_PARAM_TYPE_GET(ptypes, 0) != TEE_PARAM_TYPE_MEMREF_INOUT)
@@ -430,12 +447,37 @@ enum pkcs11_rc entry_processing_step(struct pkcs11_client *client,
 	if (rc)
 		return rc;
 
+	if (step == PKCS11_FUNC_STEP_UPDATE_KEY) {
+		assert(function == PKCS11_FUNCTION_DIGEST);
+
+		rc = serialargs_get(&ctrlargs, &key_handle, sizeof(uint32_t));
+		if (rc)
+			return rc;
+	}
+
 	if (serialargs_remaining_bytes(&ctrlargs))
 		return PKCS11_CKR_ARGUMENTS_BAD;
 
 	rc = get_active_session(session, function);
 	if (rc)
 		return rc;
+
+	if (step == PKCS11_FUNC_STEP_UPDATE_KEY) {
+		assert(function == PKCS11_FUNCTION_DIGEST);
+
+		obj = pkcs11_handle2object(key_handle, session);
+		if (!obj) {
+			rc = PKCS11_CKR_KEY_HANDLE_INVALID;
+			goto out;
+		}
+
+		rc = check_access_attrs_against_token(session,
+						      obj->attributes);
+		if (rc) {
+			rc = PKCS11_CKR_KEY_HANDLE_INVALID;
+			goto out;
+		}
+	}
 
 	mecha_type = session->processing->mecha_type;
 	rc = check_mechanism_against_processing(session, mecha_type,
@@ -446,10 +488,13 @@ enum pkcs11_rc entry_processing_step(struct pkcs11_client *client,
 	if (processing_is_tee_symm(mecha_type))
 		rc = step_symm_operation(session, function, step,
 					 ptypes, params);
+	else if (processing_is_tee_digest(mecha_type))
+		rc = step_digest_operation(session, step, obj, ptypes, params);
 	else
 		rc = PKCS11_CKR_MECHANISM_INVALID;
 
-	if (rc == PKCS11_CKR_OK && step == PKCS11_FUNC_STEP_UPDATE) {
+	if (rc == PKCS11_CKR_OK && (step == PKCS11_FUNC_STEP_UPDATE ||
+				    step == PKCS11_FUNC_STEP_UPDATE_KEY)) {
 		session->processing->updated = true;
 		DMSG("PKCS11 session%"PRIu32": processing %s %s",
 		     session->handle, id2str_proc(mecha_type),
@@ -459,6 +504,7 @@ enum pkcs11_rc entry_processing_step(struct pkcs11_client *client,
 out:
 	switch (step) {
 	case PKCS11_FUNC_STEP_UPDATE:
+	case PKCS11_FUNC_STEP_UPDATE_KEY:
 		if (rc != PKCS11_CKR_OK && rc != PKCS11_CKR_BUFFER_TOO_SMALL)
 			release_active_processing(session);
 		break;

ta/pkcs11/src/processing.h
@@ -66,4 +66,16 @@ enum pkcs11_rc tee_init_ctr_operation(struct active_processing *processing,
 enum pkcs11_rc derive_key_by_symm_enc(struct pkcs11_session *session,
 				      void **out_buf, uint32_t *out_sz);
 
+/* Digest specific functions */
+bool processing_is_tee_digest(enum pkcs11_mechanism_id mecha_id);
+
+enum pkcs11_rc
+init_digest_operation(struct pkcs11_session *session,
+		      struct pkcs11_attribute_head *proc_params);
+
+enum pkcs11_rc step_digest_operation(struct pkcs11_session *session,
+				     enum processing_step step,
+				     struct pkcs11_object *obj,
+				     uint32_t ptypes, TEE_Param *params);
+
 #endif /*PKCS11_TA_PROCESSING_H*/

commit 5abf08990f386247176c937ac44021aba0a30c53

ta: pkcs11: report unsupported digest instead of panicking
TEE_AllocateOperation() returns TEE_ERROR_NOT_SUPPORTED only for an
algorithm identifier it does not recognise. An identifier it does
recognise but whose primitive the GP TEE was built without gets past
the identifier switch, allocates the operation, and then fails in
_utee_cryp_state_alloc() with TEE_ERROR_NOT_IMPLEMENTED. That is
neither out-of-memory nor not-supported, so TEE_AllocateOperation()
panics instead of returning, and the caller never sees the mechanism
error allocate_tee_operation() is written to produce.

token_mechanism[] advertises MD5, SHA-1, SHA-224, SHA-256, SHA-384 and
SHA-512 for digesting unconditionally, so on a TEE built without one of
them a single C_DigestInit selects that mechanism and takes the TA
down.

TEE_IsAlgorithmSupported() answers from the same CFG_CRYPTO_* set that
decides whether the primitive is linked, so querying it before
allocating turns an absent digest into PKCS11_CKR_MECHANISM_INVALID.

Fixes: 9e91a619a03f ("ta: pkcs11: Add support for digest operations")
Signed-off-by: Georges Savoundararadj <savoundg@amazon.com>

ta/pkcs11/src/processing_digest.c
@@ -74,6 +74,19 @@ allocate_tee_operation(struct pkcs11_session *session,
 	if (pkcs2tee_algorithm(&algo, params))
 		return PKCS11_CKR_FUNCTION_FAILED;
 
+	/*
+	 * TEE_AllocateOperation() reports TEE_ERROR_NOT_SUPPORTED only for an
+	 * algorithm identifier it does not recognise at all. An identifier it
+	 * does recognise but whose primitive the GP TEE was built without
+	 * fails further in, where the error is neither out-of-memory nor
+	 * not-supported and TEE_AllocateOperation() panics the TA rather than
+	 * returning. Every mechanism in token_mechanism[] is reachable from
+	 * C_DigestInit, so that panic is caller-triggerable. Ask for the
+	 * primitive first and report its absence as a mechanism error.
+	 */
+	if (TEE_IsAlgorithmSupported(algo, TEE_CRYPTO_ELEMENT_NONE))
+		return PKCS11_CKR_MECHANISM_INVALID;
+
 	res = TEE_AllocateOperation(&session->processing->tee_op_handle,
 				    algo, TEE_MODE_DIGEST, 0);
 	if (res)